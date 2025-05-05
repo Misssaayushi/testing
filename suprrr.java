68977ec35ebffc9bcd5f8d5c6b0e2ee2754a2e7c
AayushiShivnaniTech <aayushishivnani716@gmail.com>
2025-05-05 18:33:09 +0530

    Create suprrr.java

---
+++ suprrr.java
@@ -0,0 +1,46 @@
+class Parent
+{
+	String name="Abc";
+	Parent(String color)
+	{
+		System.out.println("Color:"+color);
+	}
+	void display()
+	{
+		System.out.println("parent class");
+	}
+	
+}
+class Child extends Parent
+{
+	String name="chg";
+	Child()
+	{
+		super("white");
+		System.out.println("child constructor");
+	}
+	void display()
+	{
+		super.display();  //super method
+		System.out.println("child class");
+		
+	}
+	void Naam()
+	{
+		System.out.println(super.name); //super variable
+		System.out.println(name);
+	}
+
+}
+
+class suprrr
+{
+	
+	public  static void main(String[] args)
+	{
+		Child obj=new Child();
+		obj.display();
+		obj.Naam();
+	}
+
+}
